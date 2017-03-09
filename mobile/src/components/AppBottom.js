aeb0bc846e87730ad58d8a246d0a2315e0ca0bc1
zhangweiqing <zhangweiqing@migoedu.com>
2017-03-09 17:58:39 +0800

    组件开发

--- mobile/src/components/AppBottom.js
+++ mobile/src/components/AppBottom.js
@@ -31,6 +31,7 @@ export default class AppBottom extends React.Component {
 
   render() {
     return (
+        <div id="tab-bar" className="demo">
       <TabBar
         unselectedTintColor="#949494"
         tintColor="#33A3F4"
@@ -38,18 +39,18 @@ export default class AppBottom extends React.Component {
         hidden={this.state.hidden}
       >
         <TabBar.Item
-          title="生活"
-          key="生活"
+          title="我的练习"
+          key="我的练习"
           icon={<div style={{
             width: '0.44rem',
             height: '0.44rem',
-            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  0.42rem 0.42rem no-repeat' }}
+            }}
           />
           }
           selectedIcon={<div style={{
             width: '0.44rem',
             height: '0.44rem',
-            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  0.42rem 0.42rem no-repeat' }}
+            }}
           />
           }
           selected={this.state.selectedTab === 'blueTab'}
@@ -61,13 +62,13 @@ export default class AppBottom extends React.Component {
           }}
           data-seed="logId"
         >
-          {this.renderContent('生活')}
+          {this.renderContent('我的练习')}
         </TabBar.Item>
         <TabBar.Item
           icon={<Icon type="koubei-o" size="md" />}
           selectedIcon={<Icon type="koubei" size="md" />}
-          title="口碑"
-          key="口碑"
+          title="练习室"
+          key="练习室"
           badge={'new'}
           selected={this.state.selectedTab === 'redTab'}
           onPress={() => {
@@ -77,25 +78,25 @@ export default class AppBottom extends React.Component {
           }}
           data-seed="logId1"
         >
-          {this.renderContent('口碑')}
+          {this.renderContent('练习室')}
         </TabBar.Item>
         <TabBar.Item
           icon={
             <div style={{
               width: '0.44rem',
               height: '0.44rem',
-              background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  0.42rem 0.42rem no-repeat' }}
+              }}
             />
           }
           selectedIcon={
             <div style={{
               width: '0.44rem',
               height: '0.44rem',
-              background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  0.42rem 0.42rem no-repeat' }}
+              }}
             />
           }
-          title="朋友"
-          key="朋友"
+          title="学分商城"
+          key="学分商城"
           dot
           selected={this.state.selectedTab === 'greenTab'}
           onPress={() => {
@@ -104,23 +105,10 @@ export default class AppBottom extends React.Component {
             });
           }}
         >
-          {this.renderContent('朋友')}
-        </TabBar.Item>
-        <TabBar.Item
-          icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-          selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
-          title="我的"
-          key="我的"
-          selected={this.state.selectedTab === 'yellowTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'yellowTab',
-            });
-          }}
-        >
-          {this.renderContent('我的')}
+          {this.renderContent('学分商城')}
         </TabBar.Item>
       </TabBar>
+        </div>
     );
   }
 }
